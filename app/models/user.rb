01db4fdf4e868602d669d0e6b733919a57033d24
alanahanson <jepetersohn@gmail.com>
2017-02-03 16:50:19 -0600

    added username to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username, uniqueness: true, presence: true
   has_many :boasts
 end
